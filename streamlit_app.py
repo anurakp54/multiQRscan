88ee06be0c93e568f113cc3c4b2ff0c74b623e0d
anurakp54 <anurakp@gmail.com>
2022-10-30 09:09:39 +0700

    version 1.0 for use

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,14 +41,14 @@ if authentication_status == True:
     st.title('Upload QR into Database')
     temp_file = st.file_uploader("1. Upload picture in PNG or JPEG format")
     if st.button("Save to Cache"):
-        with open(Path(__file__).parent/"data/temp_file", "wb") as file_handle:
+        with open(Path(__file__).parent/"data"/"temp_file", "wb") as file_handle:
             file_handle.write(temp_file.read())
 
     try:
-        with open(Path(__file__).parent/"data/temp_file", 'rb') as image_file:
+        with open(Path(__file__).parent/"data"/"temp_file", 'rb') as image_file:
             image = Image.open(image_file)
             image.load()
-            os.remove(Path(__file__).parent/"data/temp_file")
+            os.remove(Path(__file__).parent/"data"/"temp_file")
             codes = zbarlight.scan_codes(['qrcode'], image)
 
         server = 'tcp:dcdbserverdev.database.windows.net,1433'
